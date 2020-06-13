486047af1b2909a09748102a2a6b69b89a07b630
lawjiahua <lawjiahua97@gmail.com>
2020-06-13 13:32:23 +0800

    Merge branch 'master' of https://github.com/lawjiahua/yada
    * 'master' of https://github.com/lawjiahua/yada:
      no message
      Renamed DeckScreen and DeckScreenAppBar to PlayingScreen and PlayingScreenAppBar
      Implemented routing Refactored main screen into screens/HomeScreen Changed main.dart to stateless widget Fixed bug to allow proper navigation from all decks screen to home screen
      Refactored card display from deck_screen to widgets/PlayingCardItem
      Refactored a number of things: Fixed deck.scenarioURL typo Shifted Deck and DeckCard out of CardsData into models folder Renamed DeckCard to PlayingCard
    
    # Conflicts:
    #	yada/lib/all_decks_screen.dart
    #	yada/lib/deck_screen.dart
    #	yada/lib/widgets/deck_screen_appbar.dart

--- yada/lib/widgets/deck_screen_appbar.dart
+++ yada/lib/widgets/deck_screen_appbar.dart
@@ -3,19 +3,28 @@ import 'package:flutter/material.dart';
 import '../models/deck.dart';
 import '../screens/all_decks_screen.dart';
 
-class PlayingScreenAppBar extends StatelessWidget {
+class DeckScreenAppbar extends StatefulWidget {
   final Size size;
   final Deck deck;
-  final int totalLevel = 0;
+  final int totalLevel;
+
+  const DeckScreenAppbar({this.deck, this.size, this.totalLevel});
+
+  @override
+  _DeckScreenAppbarState createState() =>
+      _DeckScreenAppbarState(totalLevel: totalLevel);
+}
+
+class _DeckScreenAppbarState extends State<DeckScreenAppbar> {
+  final int totalLevel;
   final levelDescriptions = const <String>[
-    "internet friend",
+    "Internet friend",
     'I think i know you',
-    'friend',
-    'bathroom buddies',
+    'Friend',
+    'Bathroom buddies',
     'Soulm8 4 lyf'
   ];
-
-  const PlayingScreenAppBar({this.deck, this.size});
+  _DeckScreenAppbarState({this.totalLevel});
 
   void backToDecks(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
@@ -32,29 +41,29 @@ class PlayingScreenAppBar extends StatelessWidget {
       children: <Widget>[
         Container(
           padding: EdgeInsets.only(left: 10),
-          height: size.height * 0.2,
-          width: size.width * 0.3,
+          height: widget.size.height * 0.2,
+          width: widget.size.width * 0.3,
           child: Text(
-            deck.name,
+            widget.deck.name,
             style: Theme.of(context).textTheme.headline2,
             softWrap: true,
           ),
         ),
         Container(
-          height: size.height * 0.2,
-          width: size.width * 0.4,
+          height: widget.size.height * 0.2,
+          width: widget.size.width * 0.4,
           alignment: Alignment.center,
-          padding: EdgeInsets.only(left: 10, right: size.width * 0.1),
+          padding: EdgeInsets.only(left: 10, right: widget.size.width * 0.1),
           child: Column(
             children: <Widget>[
               Container(
                 child: Text(
-                    "Level: ${levelDescriptions[(totalLevel / 5).floor()]}",
+                    "Level:  ${levelDescriptions[(totalLevel / 5).floor()]}",
                     style: Theme.of(context).textTheme.bodyText1),
               ),
               Container(
                 child: Text(
-                  "totallevel: $totalLevel",
+                  "total points:  $totalLevel",
                   style: Theme.of(context).textTheme.bodyText1,
                 ),
               ),
@@ -62,12 +71,12 @@ class PlayingScreenAppBar extends StatelessWidget {
           ),
         ),
         Container(
-          height: size.height * 0.3,
-          width: size.width * 0.2,
+          height: widget.size.height * 0.3,
+          width: widget.size.width * 0.2,
           padding: EdgeInsets.only(left: 10),
           child: FlatButton(
             child: Text(
-              "Back to Deck",
+              "Back to Decks",
               style: Theme.of(context).textTheme.headline2,
             ),
             onPressed: () {
---
+++ yada/lib/widgets/single_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
+
+import '../cards_data.dart';
+import '../models/deck.dart';
+
+class SingleCard extends StatelessWidget {
+  final Function nextCard;
+  final Function getACard;
+  final Deck deck;
+  const SingleCard({this.nextCard, this.deck, this.getACard});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: nextCard,
+      child: FlipCard(
+        front: Container(
+          height: size.height * 0.58,
+          width: size.width * 0.4,
+          child: RotatedBox(
+            quarterTurns: 5,
+            child: Image.asset(
+              deck.cardBackURL,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        back: getACard(),
+      ),
+    );
+  }
+}
